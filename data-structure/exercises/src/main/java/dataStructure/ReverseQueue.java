7579701444ed4b4ae28dd5d694a4fac5b2d66424
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-12 19:04:48 -0300

    add ReverseQueue problem solution

---
+++ data-structure/exercises/src/main/java/dataStructure/ReverseQueue.java
@@ -0,0 +1,27 @@
+package dataStructure;
+
+import java.util.Queue;
+
+public class ReverseQueue {
+    public static Queue<Integer> reverseFirstK(Queue<Integer> queue, int k) {
+        solveQueue(queue, k);
+
+        int n = queue.size() - k;
+
+        while (n-- > 0) {
+            int element = queue.remove();
+            queue.add(element);
+        }
+
+        return queue;
+    }
+
+    private static void solveQueue(final Queue<Integer> queue, int k) {
+        if (k == 0) return;
+
+        int element = queue.remove();
+
+        solveQueue(queue, k - 1);
+        queue.add(element);
+    }
+}
--- data-structure/exercises/src/main/java/main/Main.java
+++ data-structure/exercises/src/main/java/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
 import dataStructure.BracketBalancing;
+import dataStructure.ReverseQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,9 +17,9 @@ public class Main {
          * and orders of "{", "}", "(", ")" and "[", "]" are correct in the given expression.
          *
          * Solution: Stack usage
-         * 1. Stack opening characters
-         * 2. When identifying a closing character, unstack and check that it matches the opening
-         * 3. At the end, the stack should be empty
+         * 1. Stack opening characters;
+         * 2. When identifying a closing character, unstack and check that it matches the opening;
+         * 3. At the end, the stack should be empty;
          */
         BracketBalancing bracketBalancing = new BracketBalancing();
 
@@ -23,5 +27,33 @@ public class Main {
         System.out.println(bracketBalancing.solveExpression("2*(3+4+5*[2+3)]")); // false
         System.out.println(bracketBalancing.solveExpression("[2*(3+4+5*[2+3]/(2+(3+(7+5)*9)*6)*[5+8]+17)+2]")); // true
         System.out.println("######################################");
+
+        /**
+         * 2º Problem: Reverse the first K elements of a queue
+         * <p>
+         * Given an integer K and a queue of integers, the task is to reverse the order of the first k elements
+         * in the queue, leaving the other elements in the same relative order.
+         *
+         * Solution:
+         *
+         * 1. For the first K elements:
+         *  1.1. Dequeue element X;
+         *  1.2. Reorder (K - 1) elements;
+         *  1.3. Queue element X up;
+         * 2. For the N-K elements:
+         *  2.1. Dequeue;
+         *  2.2; Queue;
+         */
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println("################### REVERSE QUEUE ###################");
+        System.out.println(ReverseQueue.reverseFirstK(queue, 3));
+        System.out.println("################### ###################");
     }
 }
